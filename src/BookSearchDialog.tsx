c24ef5381b480e05681a7d11b121f8067405bb9d
kaoru-kk <kkaoru1998@gmail.com>
2021-07-28 15:46:38 +0900

    refactor

--- src/BookSearchDialog.tsx
+++ src/BookSearchDialog.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef } from "react";
+import React, {useState, useRef } from "react";
 import { BookDescription } from "./BookDescription";
 import BookSearchItem from './BookSearchItem';
 import { useBookData } from "./useBookData";
